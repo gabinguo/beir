409a4048df7c110f1b79437fb4d24ecd5a5f5da0
gabinguo <gabin.guo@gmail.com>
2022-01-26 17:32:04 +0100

    Add random examples...

--- examples/retrieval/training/collect_data_ce.py
+++ examples/retrieval/training/collect_data_ce.py
@@ -1,21 +1,26 @@
 from beir.datasets.data_loader import GenericDataLoader
 from beir.configs import dataset_stored_loc
+from beir.custom_logging import setup_logger
 from sentence_transformers import InputExample
-from typing import List
+from typing import List, Set
 from tqdm import tqdm
 import requests
 import json
 import os
-import pandas as pd
 import argparse
+import random
+import logging
+
+logger = logging.getLogger(__name__)
+setup_logger(logger)
 
 
-def collect_training_data(dataset_name: str, limit: int, split: str = "train", number_positives: int = 1, number_negatives: int = 4):
-    index_name = "beir/test"
-    docker_beir_pyserini = "http://localhost:8000"
+def collect_training_data(dataset_name: str, limit: int, split: str = "train", number_positives: int = 1,
+                          number_random_negatives: int = 2, number_hard_negatives: int = 2):
     data_folder: str = os.path.join(dataset_stored_loc, dataset_name)
     corpus, queries, qrels = GenericDataLoader(data_folder).load(split=split)
     query_ids: List[str] = list(qrels)
+    corpus_ids: List[str] = list(corpus)
 
     positive_examples: List[InputExample] = []
     negative_examples: List[InputExample] = []
@@ -29,51 +34,83 @@ def collect_training_data(dataset_name: str, limit: int, split: str = "train", n
             ))
         # collect negative examples (pyserini)
         # use query/document to retrieve relative documents
-        queries_for_search = [f"{corpus[pos_id]['title']} {query_text}" for pos_id in positive_document_ids[:number_positives]]
+        queries_for_search = [f"{corpus[pos_id]['title']} {query_text}" for pos_id in
+                              positive_document_ids[:number_positives]]
         payload = {
             "queries": queries_for_search,
             "qids": [f"{query_id}_{idx + 1}" for idx in range(len(queries_for_search))],
-            "k": number_negatives + 1
+            "k": 100
         }
         hits = json.loads(
             requests.post(docker_beir_pyserini + "/lexical/batch_search/", json=payload).text)["results"]
 
-        count: int = 0
+        cnt_hard_negatives: int = 0
+        cnt_random_negatives: int = 0
+        record_hit_ids: List[str] = []
+        # Block to add hard negatives
         for query_for_search in range(len(positive_document_ids)):
             if f"{query_id}_{query_for_search + 1}" in hits:
-                for hit_id in hits[f"{query_id}_{query_for_search + 1}"]:
-                    if hit_id not in positive_document_ids and count < number_negatives:
+                hit_ids = list(hits[f"{query_id}_{query_for_search + 1}"])[:100]
+                record_hit_ids.extend(hit_ids)
+                for hit_id in hit_ids:
+                    if hit_id not in positive_document_ids and cnt_hard_negatives < number_hard_negatives:
                         negative_document = corpus[hit_id]["title"] + " " + corpus[hit_id]["text"]
                         negative_examples.append(InputExample(
                             texts=[query_text, negative_document], label=0
                         ))
-                        count += 1
+                        cnt_hard_negatives += 1
 
-    curr_folder = os.path.abspath(os.path.dirname(__file__))
-    os.makedirs(os.path.join(curr_folder, dataset_name), exist_ok=True)
-    with open(os.path.join(curr_folder, dataset_name, f"{dataset_name}_train_{limit}.jsonl"), 'w') as f:
-        for example in [*positive_examples, *negative_examples]:
-            f.write(json.dumps({
-                "query": example.texts[0],
-                "document": example.texts[1],
-                "label": example.label
-            }))
-            f.write("\n")
+        record_hit_ids: Set[str] = set(record_hit_ids)
+
+        # Block to add random negatives
+        while cnt_random_negatives < number_random_negatives:
+            random_document_id = random.choice(corpus_ids)
+            while random_document_id in record_hit_ids:
+                logger.info("Overlapped with hard examples... random pick another one...")
+                random_document_id = random.choice(corpus_ids)
+            random_negative_document = corpus[random_document_id]["title"] + " " + corpus[random_document_id]["text"]
+            negative_examples.append(InputExample(
+                texts=[query_text, random_negative_document], label=0
+            ))
+
+        curr_folder = os.path.abspath(os.path.dirname(__file__))
+        os.makedirs(os.path.join(curr_folder, dataset_name), exist_ok=True)
+        with open(os.path.join(curr_folder, dataset_name, output_ds_name),
+                  'w') as f:
+            for example in [*positive_examples, *negative_examples]:
+                f.write(json.dumps({
+                    "query": example.texts[0],
+                    "document": example.texts[1],
+                    "label": example.label
+                }))
+                f.write("\n")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str)
     parser.add_argument("--split", type=str, default="train")
     parser.add_argument("--limit", type=int, default=100)
+    parser.add_argument("--port", type=int, default=8000)
     parser.add_argument("--number_positives", type=int, default=1)
-    parser.add_argument("--number_negatives", type=int, default=4)
+    parser.add_argument("--number_random_negatives", type=int, default=2)
+    parser.add_argument("--number_hard_negatives", type=int, default=2)
 
     params = parser.parse_args()
+    dataset = params.dataset
+    split = params.split
+    limit = params.limit
+    num_pos = params.number_positives
+    num_rands = params.number_random_negatives
+    num_hards = params.number_hard_negatives
 
+    output_ds_name: str = f"{dataset}_{split}_{limit}_rand_{num_rands}_hard_{num_hards}.jsonl"
+    docker_beir_pyserini = f"http://localhost:{params.port}"
     collect_training_data(
-        dataset_name=params.dataset,
-        limit=params.limit,
-        split=params.split,
-        number_positives=params.number_positives,
-        number_negatives=params.number_negatives,
+        dataset_name=dataset,
+        limit=limit,
+        split=split,
+        number_positives=num_pos,
+        number_random_negatives=num_rands,
+        number_hard_negatives=num_hards
     )
