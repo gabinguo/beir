8dde66657a26704d94d9246ce5d0076b78477031
gabinguo <gabin.guo@gmail.com>
2022-02-14 15:33:38 +0100

    better imports.

--- examples/retrieval/evaluation/dense/evaluate_dpr.py
+++ examples/retrieval/evaluation/dense/evaluate_dpr.py
@@ -1,6 +1,6 @@
 from beir.configs import dataset_stored_loc
 from beir.custom_logging import setup_logger
-from beir.retrieval import models
+from beir.retrieval.models import SentenceBERT
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
 from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split=params.split)
 
-    model = DRES(models.SentenceBERT((
+    model = DRES(SentenceBERT((
         params.query_encoder,
         params.context_encoder,
         " [SEP] "), batch_size=params.batch_size))
@@ -39,14 +39,3 @@ if __name__ == '__main__':
 
     logger.info("Retriever evaluation for k in: {}".format(retriever.k_values))
     ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
-    # #### Print top-k documents retrieved ####
-    # top_k = 10
-    #
-    # query_id, ranking_scores = random.choice(list(results.items()))
-    # scores_sorted = sorted(ranking_scores.items(), key=lambda item: item[1], reverse=True)
-    # logging.info("Query : %s\n" % queries[query_id])
-    #
-    # for rank in range(top_k):
-    #     doc_id = scores_sorted[rank][0]
-    #     # Format: Rank x: ID [Title] Body
-    #     logging.info("Rank %d: %s [%s] - %s\n" % (rank+1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
